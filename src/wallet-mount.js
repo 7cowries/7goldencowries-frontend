204994fdff468a2bc545a8087c0fdc8ea6842e0e
7cowries <7goldencowries@gmail.com>
2025-09-30 02:26:50 +0100

    chore(build): add stub src/wallet-mount.js to avoid missing import during deploy

---
+++ src/wallet-mount.js
@@ -0,0 +1,8 @@
+// src/wallet-mount.js
+// Stub file to prevent build errors while migrating wallet mount to be inside TonConnectUIProvider.
+// Previously this file mounted a wallet UI into #wallet-root which caused the TonConnect provider
+// warning because it ran outside the provider. We created a stub to allow CI/deploy to succeed.
+// If you want to remove this, find the import that references './wallet-mount' and delete it.
+export default function noopWalletMount() {
+  /* no-op stub */
+}
