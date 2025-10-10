b511f9035be82d5f8cb6abcb2dc5d4fefd386e20
7cowries <7goldencowries@gmail.com>
2025-10-10 03:12:14 +0100

    fix(wallet): remove stray re-export to satisfy ESLint/Babel

--- src/hooks/useWallet.js
+++ src/hooks/useWallet.js
@@ -74,4 +74,3 @@ export const connect = (manifestUrl) => connectWallet(manifestUrl);
 export const disconnect = () => disconnectWallet();
 // export default useWallet;
 
-export { connect, disconnect };
