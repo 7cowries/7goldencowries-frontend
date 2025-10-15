0871e20dff0683e4f18c3653e8998ac1ab2393a1
7cowries <7goldencowries@gmail.com>
2025-10-15 02:12:22 +0100

    build: ignore ESLint during Next build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  // If you ever add TS, you can also opt out of TS build errors with:
-  // typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true }
 };
 module.exports = nextConfig;
