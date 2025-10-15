c1af45216d00c39569c46632ddb98828eb7998cc
7cowries <7goldencowries@gmail.com>
2025-10-15 03:03:10 +0100

    chore: ensure TonConnectUIProvider wraps app + add manifest

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,14 @@
-import '../styles.css';
+import '@/styles.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import { useMemo } from 'react';
 
-function Provider({ children }) {
-  // Build the manifest URL reliably in SSR/CSR
-  const manifestUrl = useMemo(() => {
-    const base =
-      (typeof window === 'undefined'
-        ? (process.env.NEXT_PUBLIC_SITE_URL || 'https://7goldencowries.com')
-        : window.location.origin);
-    return `${base}/tonconnect-manifest.json`;
-  }, []);
+export default function App({ Component, pageProps }) {
+  const site = process.env.NEXT_PUBLIC_SITE_URL || '';
+  // TonConnect manifest must be absolute
+  const manifestUrl = site.replace(/\/$/, '') + '/tonconnect-manifest.json';
+
   return (
     <TonConnectUIProvider manifestUrl={manifestUrl}>
-      {children}
-    </TonConnectUIProvider>
-  );
-}
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <Provider>
       <Component {...pageProps} />
-    </Provider>
+    </TonConnectUIProvider>
   );
 }
