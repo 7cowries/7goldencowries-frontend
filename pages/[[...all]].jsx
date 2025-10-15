036fc03c1f26a6554dc40ec76cb9bfa0ea9c79e1
7cowries <7goldencowries@gmail.com>
2025-10-15 18:24:19 +0100

    chore(debug): show client-side errors on page

--- pages/[[...all]].jsx
+++ pages/[[...all]].jsx
@@ -1,6 +1,25 @@
+import React from 'react';
 import dynamic from 'next/dynamic';
-const AppNoSSR = dynamic(() => import('../src/App'), { ssr: false });
 
-export default function CatchAll() {
-  return <AppNoSSR />;
+const AppNoSSR = dynamic(() => import('../src/App'), {
+  ssr: false,
+  loading: () => <p style={{color:"#fff",padding:"1rem"}}>Loading app…</p>,
+});
+
+class ErrorBoundary extends React.Component {
+  constructor(props){ super(props); this.state = { error: null }; }
+  static getDerivedStateFromError(error){ return { error }; }
+  componentDidCatch(error, info){ console.error('App error:', error, info); }
+  render(){
+    if (this.state.error) {
+      return (
+        <pre style={{color:"#fff",background:"#111",padding:"1rem",whiteSpace:"pre-wrap"}}>
+{String(this.state.error)}
+        </pre>
+      );
+    }
+    return this.props.children;
+  }
 }
+
+export default function CatchAll(){ return <ErrorBoundary><AppNoSSR/></ErrorBoundary>; }
