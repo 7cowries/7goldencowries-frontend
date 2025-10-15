9580d5f92c5b51d13b594ac37141cec4b8fae5a6
7cowries <7goldencowries@gmail.com>
2025-10-15 12:12:28 +0100

    feat(wallet): wrap App with TonConnectUIProvider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const manifestUrl = process.env.REACT_APP_TONCONNECT_MANIFEST_URL;
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </TonConnectUIProvider>
   </React.StrictMode>
 );
