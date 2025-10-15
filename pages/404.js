f19064e64067ccf0c86081d70da0c768ecf6dbdb
7cowries <7goldencowries@gmail.com>
2025-10-15 02:18:19 +0100

    ux: add custom 404/500 pages

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main style={{padding:"4rem",textAlign:"center"}}>
+      <h1>Page not found</h1>
+      <p><a href="/">Go home</a></p>
+    </main>
+  );
+}
---
+++ pages/500.js
@@ -0,0 +1,8 @@
+export default function ServerError() {
+  return (
+    <main style={{padding:"4rem",textAlign:"center"}}>
+      <h1>Something went wrong</h1>
+      <p>Please retry in a moment.</p>
+    </main>
+  );
+}
