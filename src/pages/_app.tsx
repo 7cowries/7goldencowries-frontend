c9855f7cbad9eb956eaf28fa73f1a903172020cc
7cowries <7goldencowries@gmail.com>
2025-10-16 19:27:49 +0100

    app: enforce global CSS via _app.tsx and client-only TonProvider; add sane globals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
 import dynamic from 'next/dynamic';
+import '../styles/globals.css';
 
+// Load context/TonProvider on client only
 const TonProvider = dynamic(() => import('../context/TonProvider').then(m => m.default), { ssr: false });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
