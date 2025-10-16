99799dbc9b421bfe049e2cd380e8862ee8c9de8b
7cowries <7goldencowries@gmail.com>
2025-10-16 18:45:01 +0100

    ui: restore global CSS import in _app; keep TonProvider and global connect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
+
 import { TonProvider } from '../src/context/TonProvider';
 
 const WalletConnect = dynamic(() => import('../src/components/WalletConnect'), { ssr: false });
@@ -8,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <TonProvider>
       <Component {...pageProps} />
-      {/* site-wide fallback connect button */}
+      {/* Site-wide fallback connect (hidden visually until needed) */}
       <div style={{ position:'fixed', left:12, bottom:12, zIndex:1000 }}>
         <WalletConnect compact />
       </div>
