573f119f4a0433ca4a5eafae9842269828a3a830
7cowries <7goldencowries@gmail.com>
2025-10-16 12:11:39 +0100

    fix(build): de-duplicate React import in ClientApp.jsx

--- src/ClientApp.jsx
+++ src/ClientApp.jsx
@@ -1,7 +1,6 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import React from 'react';
-import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App"; // your existing app component
 
