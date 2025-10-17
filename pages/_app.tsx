20b9d05c6fc59813aabe4186ab12d393d721088b
7cowries <7goldencowries@gmail.com>
2025-10-17 09:48:46 +0100

    build: remove duplicate pages/_app.js; normalize globals.css import; keep client-only TonProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
-import '../src/styles/globals.css';
+import '../styles/globals.css';
 
 const TonProvider = dynamic(
   () => import('../src/context/TonProvider').then(m => m.default),
