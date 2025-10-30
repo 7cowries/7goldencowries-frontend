74111a797141a17b870799453504d1755fe2dfed
7cowries <7goldencowries@gmail.com>
2025-10-30 04:00:46 +0100

    fix: restore Next _app without CRA src imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,17 @@
 import Head from 'next/head';
-import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-import "../styles/globals.css";
-
-/* Load ONLY global styles here */
-import '../src/App.css';
-import '../src/pages/Landing.css';
-import '../src/pages/Leaderboard.css';
-import '../src/pages/Quests.css';
-import '../src/pages/Isles.css';
-import '../src/pages/Referral.css';
-import '../src/pages/Profile.css';
-import '../src/pages/Subscription.css';
-import '../src/pages/TokenSale.css';
-import '../src/components/ConnectButtons.css';
-import '../src/components/XPModal.css';
-import '../src/components/layout/Sidebar.css';
-
-import { WalletProvider } from '../src/context/WalletProvider';
-
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }) {
   return (
-    <WalletProvider>
-      <>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="#071f34" />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="icon" href="/favicon.ico" />
-          <title>7 Golden Cowries</title>
-        </Head>
-        <Component {...pageProps} />
-      </>
-    </WalletProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#071f34" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>7 Golden Cowries</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
   );
 }
