2f3a59102ff06d40b02f8d7e5d13bc2e1573d155
7cowries <7goldencowries@gmail.com>
2025-10-30 04:54:04 +0100

    fix: add WalletProvider bridge for restored CRA UI

--- src/context/WalletProvider.js
+++ src/context/WalletProvider.js
@@ -1,9 +1,2 @@
-// bridge file because pages/_app.tsx imports ../src/context/WalletProvider
-// but the actual implementation lives in WalletContext.js
-
 export * from './WalletContext.js';
-
-// if WalletContext.js has `export const WalletProvider = ...`
-// this makes `import { WalletProvider } ...` work
-// and also `import WalletProvider from ...` work
 export { WalletProvider as default } from './WalletContext.js';
