8242e86a0c0889d1bdfc8dcfec3b7eb163429452
7cowries <7goldencowries@gmail.com>
2025-11-08 23:01:46 +0100

    Fix import: use @/components/SessionSync and ensure @/* alias

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import SessionSync from '@/src/components/SessionSync';
+import SessionSync from '@/components/SessionSync';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
